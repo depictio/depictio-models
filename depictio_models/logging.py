03a55ac3a629cdc3610c1324225990eadebfffa8
Thomas Weber <weber8thomas@gmail.com>
2025-01-24 16:17:58 +0100

    refactor: restructure logging setup to allow dynamic configuration and prevent handler duplication

--- depictio_models/logging.py
+++ depictio_models/logging.py
@@ -1,31 +1,36 @@
+# depictio_cli/logging.py
 import logging
 from colorlog import ColoredFormatter
 
-
-
-# Create a colored formatter
-formatter = ColoredFormatter(
-    "%(log_color)s%(asctime)s%(reset)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
-        'DEBUG':    'cyan',
-        'INFO':     'green',
-        'WARNING':  'yellow',
-        'ERROR':    'red',
-        'CRITICAL': 'red,bg_white',
-    },
-    secondary_log_colors={},
-    style='%'
-)
-
-# Create a console handler and set the formatter
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-# Create a logger
-logging.basicConfig(level=logging.DEBUG, handlers=[handler])
-
-# Add the handler to the logger
+# Initialize logger without handlers
 logger = logging.getLogger("depictio-models")
+logger.propagate = False  # Prevent propagation to root logger
 
+def setup_logging(verbose: bool = False) -> logging.Logger:
+    global logger
+    
+    # Clear any existing handlers
+    logger.handlers.clear()
+    
+    if verbose:
+        formatter = ColoredFormatter(
+            "%(log_color)s%(asctime)s%(reset)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s",
+            datefmt=None,
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+            }
+        )
+        
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARNING)
+        
+    return logger
