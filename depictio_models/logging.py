c773296a3565cfd643b7d6002fe659c574643625
Thomas Weber <weber8thomas@gmail.com>
2025-01-30 11:33:17 +0100

    feat: enhance logging setup with configurable verbose level

--- depictio_models/logging.py
+++ depictio_models/logging.py
@@ -6,31 +6,32 @@ from colorlog import ColoredFormatter
 logger = logging.getLogger("depictio-models")
 logger.propagate = False  # Prevent propagation to root logger
 
-def setup_logging(verbose: bool = False) -> logging.Logger:
+
+def setup_logging(verbose: bool = False, verbose_level: str = "INFO") -> logging.Logger:
     global logger
-    
+
     # Clear any existing handlers
     logger.handlers.clear()
-    
+    formatter = ColoredFormatter(
+        "%(log_color)s%(asctime)s%(reset)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     if verbose:
-        formatter = ColoredFormatter(
-            "%(log_color)s%(asctime)s%(reset)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s",
-            datefmt=None,
-            reset=True,
-            log_colors={
-                'DEBUG': 'cyan',
-                'INFO': 'green',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'red,bg_white',
-            }
-        )
-        
-        handler = logging.StreamHandler()
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(logging.DEBUG)
+        level_name = logging.getLevelNamesMapping().get(verbose_level, verbose_level)
+        logger.setLevel(level_name)
     else:
         logger.setLevel(logging.WARNING)
-        
+
     return logger
