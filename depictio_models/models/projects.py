857de328f5117575f7ea2c04fdcf867f33465b48
Thomas Weber <weber8thomas@gmail.com>
2025-04-08 09:58:06 +0200

    refactor: change Project class inheritance from MongoModel to Document and clean up registration_time handling

--- depictio_models/models/projects.py
+++ depictio_models/models/projects.py
@@ -2,17 +2,18 @@ from datetime import datetime
 import os
 import re
 from typing import List, Optional
+from beanie import Document
 from pydantic import field_validator, model_validator
 import hashlib
 import json
 
 from depictio_models.models.users import Permission
 from depictio_models.models.workflows import Workflow
-from depictio_models.models.base import MongoModel, convert_objectid_to_str
+from depictio_models.models.base import convert_objectid_to_str
 from depictio_models.config import DEPICTIO_CONTEXT
 
 
-class Project(MongoModel):
+class Project(Document):
     name: str
     data_management_platform_project_url: Optional[str] = None
     workflows: List[Workflow]
@@ -21,6 +22,9 @@ class Project(MongoModel):
     is_public: bool = False
     hash: Optional[str] = None
     registration_time: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    class Settings:
+        name = "projects"
 
     @field_validator("name")
     @classmethod
@@ -37,9 +41,14 @@ class Project(MongoModel):
         # Compute the hash of the project configuration after removing all the "registration_time" fields in project and nested objects
         values.pop("registration_time", None)
         for workflow in values["workflows"]:
-            workflow.pop("registration_time", None)
+            if workflow.get("registration_time"):
+                # Remove registration_time from workflow and its data_collections
+                workflow.pop("registration_time", None)
             for data_collection in workflow["data_collections"]:
-                data_collection.pop("registration_time", None)
+                if data_collection.get("registration_time"):
+                    # Remove registration_time from data_collection
+                    data_collection.pop("registration_time", None)
+                # data_collection.pop("registration_time", None)
 
         hash_str = hashlib.md5(
             json.dumps(convert_objectid_to_str(values), sort_keys=True).encode()
@@ -64,6 +73,8 @@ class Project(MongoModel):
     @classmethod
     def validate_data_management_platform_project_url(cls, v):
         # Check if looks like a valid URL
+        if not v:
+            return v
         if not re.match(r"https?://", v):
             raise ValueError("Invalid URL")
         return v
