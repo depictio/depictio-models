48823f650c88552c1a7e265cdaad5be9bd3d60f5
Thomas Weber <weber8thomas@gmail.com>
2025-04-09 14:26:02 +0200

    refactor: change Project class inheritance from Document to MongoModel and clean up unused code ; disable temporarily hash function

--- depictio_models/models/projects.py
+++ depictio_models/models/projects.py
@@ -3,17 +3,15 @@ import os
 import re
 from typing import List, Optional
 from beanie import Document
-from pydantic import field_validator, model_validator
-import hashlib
-import json
+from pydantic import field_validator
 
 from depictio_models.models.users import Permission
 from depictio_models.models.workflows import Workflow
-from depictio_models.models.base import convert_objectid_to_str
+from depictio_models.models.base import MongoModel
 from depictio_models.config import DEPICTIO_CONTEXT
 
 
-class Project(Document):
+class Project(MongoModel):
     name: str
     data_management_platform_project_url: Optional[str] = None
     workflows: List[Workflow]
@@ -23,9 +21,6 @@ class Project(Document):
     hash: Optional[str] = None
     registration_time: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    class Settings:
-        name = "projects"
-
     @field_validator("name")
     @classmethod
     def validate_name(cls, v):
@@ -33,28 +28,28 @@ class Project(Document):
             raise ValueError("Project name cannot be empty")
         return v
 
-    @model_validator(mode="before")
-    def compute_hash(cls, values: dict) -> dict:
-        """
-        Compute the hash of the project configuration.
-        """
-        # Compute the hash of the project configuration after removing all the "registration_time" fields in project and nested objects
-        values.pop("registration_time", None)
-        for workflow in values["workflows"]:
-            if workflow.get("registration_time"):
-                # Remove registration_time from workflow and its data_collections
-                workflow.pop("registration_time", None)
-            for data_collection in workflow["data_collections"]:
-                if data_collection.get("registration_time"):
-                    # Remove registration_time from data_collection
-                    data_collection.pop("registration_time", None)
-                # data_collection.pop("registration_time", None)
+    # @model_validator(mode="before")
+    # def compute_hash(cls, values: dict) -> dict:
+    #     """
+    #     Compute the hash of the project configuration.
+    #     """
+    #     # Compute the hash of the project configuration after removing all the "registration_time" fields in project and nested objects
+    #     values.pop("registration_time", None)
+    #     for workflow in values["workflows"]:
+    #         if workflow.get("registration_time"):
+    #             # Remove registration_time from workflow and its data_collections
+    #             workflow.pop("registration_time", None)
+    #         for data_collection in workflow["data_collections"]:
+    #             if data_collection.get("registration_time"):
+    #                 # Remove registration_time from data_collection
+    #                 data_collection.pop("registration_time", None)
+    #             # data_collection.pop("registration_time", None)
 
-        hash_str = hashlib.md5(
-            json.dumps(convert_objectid_to_str(values), sort_keys=True).encode()
-        ).hexdigest()
-        values["hash"] = hash_str
-        return values
+    #     hash_str = hashlib.md5(
+    #         json.dumps(convert_objectid_to_str(values), sort_keys=True).encode()
+    #     ).hexdigest()
+    #     values["hash"] = hash_str
+    #     return values
 
     @field_validator("yaml_config_path")
     @classmethod
@@ -78,3 +73,8 @@ class Project(Document):
         if not re.match(r"https?://", v):
             raise ValueError("Invalid URL")
         return v
+
+
+class ProjectBeanie(Project, Document):
+    class Settings:
+        name = "projects"
